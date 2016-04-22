e2e01df1455b7699e1678279f3630877dc73e5f1
Gonzalo Peci <pecigonzalo@outlook.com>
2016-04-22 11:55:37 +0200

    Simplify and improve rate limiting config

--- scripts/setup_limit.sh
+++ scripts/setup_limit.sh
@@ -1,66 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
 ## Paths and definitions
 tc=/sbin/tc
-ext=eth2    # Change for your device!
-ext_ingress=ifb0  # Use a unique ifb per rate limiter!
-      # Set these as per your provider's settings, at 90% to start with
-ext_up=800kbit    # Max theoretical: for this example, up is 1024kbit
-ext_down=7100kbit # Max theoretical: for this example, down is 8192kbit
-q=1514                  # HTB Quantum = 1500bytes IP + 14 bytes ethernet.
-      # Higher bandwidths may require a higher htb quantum. MEASURE.
-      # Some ADSL devices might require a stab setting.
 
-quantum=300   # fq_codel quantum 300 gives a boost to interactive flows
-      # At higher bandwidths (50Mbit+) don't bother
+int=eth1
+int_real_speed=1000
+int_link_speed=$(echo "$int_real_speed * 0.95" | bc | xargs printf "%.0fmbit" )
+int_link_speed_half=$(echo "$int_real_speed * 0.5" | bc | xargs printf "%.0fmbit" )
+int_limit=1000kbit # Rated down
+
+ext=eth2
+ext_real_speed=1000
+ext_link_speed=$(echo "$ext_real_speed * 0.95" | bc | xargs printf "%.0fmbit" )
+ext_link_speed_half=$(echo "$ext_real_speed * 0.5" | bc | xargs printf "%.0fmbit" )
+ext_limit=1000kbit    # Rated up
 
 
-modprobe ifb
-modprobe sch_fq_codel
-modprobe act_mirred
+# #########
+# # INGRESS
+# #########
 
-# Clear old queuing disciplines (qdisc) on the interfaces
+# Delete all qdiscs
+$tc qdisc del dev $int root
+
+# Enable HTB qdisc
+$tc qdisc replace dev $int root handle 1: htb default 20
+
+$tc class add dev $int parent 1: classid 1:1 htb rate $int_link_speed
+
+$tc class add dev $int parent 1:1 classid 1:10 htb rate 1mbit ceil $int_link_speed_half prio 1
+$tc class add dev $int parent 1:1 classid 1:20 htb rate $int_limit ceil $int_limit prio 2
+$tc class add dev $int parent 1:1 classid 1:30 htb rate 1mbit ceil $int_link_speed prio 3
+
+tc qdisc add dev $int parent 1:10 fq_codel
+tc qdisc add dev $int parent 1:20 fq_codel
+tc qdisc add dev $int parent 1:30 fq_codel
+
+# #########
+# # EGRESS
+# #########
+
 $tc qdisc del dev $ext root
-$tc qdisc del dev $ext ingress
-$tc qdisc del dev $ext_ingress root
-$tc qdisc del dev $ext_ingress ingress
 
-#########
-# INGRESS
-#########
+$tc qdisc replace dev $ext root handle 1: htb default 20
 
-# Create ingress on external interface
-$tc qdisc add dev $ext handle ffff: ingress
+$tc class add dev $ext parent 1: classid 1:1 htb rate $ext_link_speed
 
-# Forward all ingress traffic to the IFB device
-$tc filter add dev $ext parent ffff: protocol all u32 match u32 0 0 action mirred egress redirect dev $ext_ingress
+$tc class add dev $ext parent 1:1 classid 1:10 htb rate 1mbit ceil $ext_link_speed_half prio 1
+$tc class add dev $ext parent 1:1 classid 1:20 htb rate $ext_limit ceil $int_limit prio 2
+$tc class add dev $ext parent 1:1 classid 1:30 htb rate 1mbit ceil $ext_link_speed prio 3
 
-# Create an EGRESS filter on the IFB device
-$tc qdisc add dev $ext_ingress root handle 1: htb default 11
-
-# Add root class HTB with rate limiting
-
-$tc class add dev $ext_ingress parent 1: classid 1:1 htb rate $ext_down
-$tc class add dev $ext_ingress parent 1:1 classid 1:11 htb rate $ext_down prio 0 quantum $q
-
-
-# Add FQ_CODEL qdisc with ECN support (if you want ecn)
-$tc qdisc add dev $ext_ingress parent 1:11 fq_codel quantum $quantum ecn
-
-#########
-# EGRESS
-#########
-# Add FQ_CODEL to EGRESS on external interface
-$tc qdisc add dev $ext root handle 1: htb default 11
-
-# Add root class HTB with rate limiting
-$tc class add dev $ext parent 1: classid 1:1 htb rate $ext_up
-$tc class add dev $ext parent 1:1 classid 1:11 htb rate $ext_up prio 0 quantum $q
-
-# Note: You can apply a packet limit here and on ingress if you are memory constrained - e.g
-# for low bandwidths and machines with < 64MB of ram, limit 1000 is good, otherwise no point
-
-# Add FQ_CODEL qdisc without ECN support - on egress it's generally better to just drop the packet
-# but feel free to enable it if you want.
-
-$tc qdisc add dev $ext parent 1:11 fq_codel quantum $quantum noecn
+tc qdisc add dev $ext parent 1:10 fq_codel
+tc qdisc add dev $ext parent 1:20 fq_codel
+tc qdisc add dev $ext parent 1:30 fq_codel
--- scripts/setup_unlimit.sh
+++ scripts/setup_unlimit.sh
@@ -10,3 +10,12 @@ $tc qdisc del dev $ext root
 $tc qdisc del dev $ext ingress
 $tc qdisc del dev $ext_ingress root
 $tc qdisc del dev $ext_ingress ingress
+
+ext=eth1    # Change for your device!
+ext_ingress=ifb0  # Use a unique ifb per rate limiter!
+
+# Clear old queuing disciplines (qdisc) on the interfaces
+$tc qdisc del dev $ext root
+$tc qdisc del dev $ext ingress
+$tc qdisc del dev $ext_ingress root
+$tc qdisc del dev $ext_ingress ingress
